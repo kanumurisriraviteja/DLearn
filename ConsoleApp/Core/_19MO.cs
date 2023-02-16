b35c1f9d221c1507cd003513f4951a3084d27dec
ksrt <kanumuri.sriraviteja@outlook.com>
2023-02-16 08:56:23 +0530

    method overloading

---
+++ ConsoleApp/Core/_19MO.cs
@@ -0,0 +1,37 @@
+namespace ConsoleApp.Core;
+
+class _19MO
+{
+     public static void Learn()
+     {
+            Add(5,5,4);     
+            Add(5,5,5);
+
+     }
+
+           public static double Add(float a,int b,int c)
+            {
+                System.Console.WriteLine("1 is executed");
+                return a+b;
+            }
+
+            public static float Add(int a,int b,int c)
+            {
+                System.Console.WriteLine("2 is executed");
+                Console.WriteLine(a+b+c);
+                return a+b+c;
+            }
+
+            public static float Add(int a,int b)
+            {
+                System.Console.WriteLine("3 is executed");
+                return a+b;
+            }
+
+            public static float Add(int a,int b,int c,int d=1)
+            {
+                  System.Console.WriteLine("4 is executed");
+                return a+b;
+            }
+
+}
