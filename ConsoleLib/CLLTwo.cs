de04b5245cf816af7f2f953a7166713737bf5d00
ksrt <kanumuri.sriraviteja@outlook.com>
2023-03-13 06:44:15 +0530

    changes done

--- ConsoleLib/CLLTwo.cs
+++ ConsoleLib/CLLTwo.cs
@@ -12,17 +12,22 @@ namespace ConsoleLib
         public void Learn()
         {
             _4Nsl o1 = new _4Nsl();
-            Console.WriteLine(o1.a1); // Accessable
-            //Console.WriteLine(o1.a2); // Error Priavte not Accessable
-            //Console.WriteLine(o1.a3); // Error protected not accesable
+            Console.WriteLine(o1.a1); // public accessable
+            //Console.WriteLine(o1.a2); // Error Private not Accessable
+            // Console.WriteLine(o1.a3); // Error protected not accesable
             //Console.WriteLine(o1.a4); // Error internal not accesable
-
-            Console.WriteLine(a5); // Accesable
+            //Console.WriteLine(o1.a5); // Error not accesable via the object
+ 
             CLLTwo cLL = new CLLTwo();
+            Console.WriteLine(cLL.a1); // public accesable
+            // Console.WriteLine(cLL.a2); // Error Private not Accessable
+            Console.WriteLine(cLL.a3); // protected Accesable
+            // Console.WriteLine(cLL.a4); // Error internal not Accesable
             Console.WriteLine(cLL.a5); // Accesable
             Console.WriteLine(base.a5); // Accesable
+            Console.WriteLine(a5); // Accesable
 
-            //Console.WriteLine(o1.a5); // Error not accesable via the object
+           
 
         }
     }
